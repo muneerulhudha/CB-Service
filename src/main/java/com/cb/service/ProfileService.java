ef251652f4489e262b1b8400fed4f7c9bfb8cfee
Muneer Hudha <muneerulhudha@gmail.com>
2016-05-01 19:20:45 -0500

    profile update added

--- src/main/java/com/cb/service/ProfileService.java
+++ src/main/java/com/cb/service/ProfileService.java
@@ -1,8 +1,15 @@
 package com.cb.service;
 
+import static com.mongodb.client.model.Filters.eq;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.bson.Document;
 
@@ -11,7 +18,6 @@ import com.cb.model.Profile;
 import com.cb.util.Util;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import static com.mongodb.client.model.Filters.*;
 
 @Path("profile")
 public class ProfileService {
@@ -29,6 +35,30 @@ public class ProfileService {
 		Profile profile = new Profile(doc.get("username").toString(), doc.get("name").toString(), doc.get("password").toString(), doc.get("email").toString(), doc.get("dept").toString(), doc.get("address").toString(), doc.get("phoneno").toString(), doc.get("lastLoginTime").toString());
 		
 		return Util.toJson(profile);
+	}
+	
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateProfile(@FormParam("username") String username, @FormParam("email") String email, @FormParam("name") String name, @FormParam("address") String address, @FormParam("phoneno") String phoneno, @FormParam("dept") String dept){
+		
+		DBConnector dbInstance = DBConnector.getInstance();
+		MongoDatabase dbCourseBook = dbInstance.getDatabase();
+		MongoCollection<Document> userCollection = dbCourseBook.getCollection("user_profile");
+		
+		Document existingDoc = userCollection.find(eq("username", username)).first();
+		
+		Document doc = new Document("username", username)
+					.append("password", existingDoc.get("password"))
+					.append("email", email)
+					.append("name", name)
+					.append("dept", dept)
+	                .append("address", address)
+	                .append("phoneno", phoneno);
+		
+		userCollection.updateOne(eq("username", username), new Document("$set", doc));
+		
+		return "{\"success\": \"true\"}";
 	}
 	
 }
